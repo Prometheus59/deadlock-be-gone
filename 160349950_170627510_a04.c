0da60c8d1fb8a4082e96db133e2340080c627247
Ryan Karumanchery <ryankarumanchery@gmail.com>
2020-07-19 20:00:32 -0400

    Added inital variables/function headers

--- 160349950_170627510_a04.c
+++ 160349950_170627510_a04.c
@@ -1,3 +1,16 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+// Get from user input
+int m = 5;
+int n = 5;
+
+int available[m];
+int max[n][m];
+int allocation[n][m];
+int need[n][m];
+
+int request_res();
+int release_res();
+int safety_algorithm();

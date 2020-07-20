38b4d3ed798b4ddcc320fbd4d2f4bb55132eabd0
Ryan Karumanchery <ryankarumanchery@gmail.com>
2020-07-20 19:44:00 -0400

    Input parsable

--- 160349950_170627510_a04.c
+++ 160349950_170627510_a04.c
@@ -9,11 +9,18 @@ int release_res();
 int safety_algorithm();
 
 int main(int argc, char* argv[]) {
-    int m = argc - 1;
-    int n = 5;  // idk what this does
+    int resource_count = argc - 1;
+    // int customer_count = 5;
 
-    int available[m];
-    int max[n][m];
-    int allocation[n][m];
-    int need[n][m];
+    int available[resource_count];
+    for (int x = 0; x < resource_count; x++) {
+        int num = strtol(argv[x + 1], NULL, 10);
+        available[x] = num;
+    }
+    for (int x = 0; x < resource_count; x++) {
+        printf("%d\n", available[x]);
+    }
+    // int max[customer_count][resource_count];
+    // int allocation[customer_count][resource_count];
+    // int need[customer_count][resource_count];
 }

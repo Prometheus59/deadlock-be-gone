0d9cc4ab6ac3a0b30055c2580c537eeab0c5c047
Raman-Saini <saini.ramandeep19@gmail.com>
2020-07-28 20:43:17 -0400

    Updated readFile

--- 160349950_170627510_a04.c
+++ 160349950_170627510_a04.c
@@ -43,24 +43,31 @@ int main(int argc, char* argv[]) {
 }
 
 int readFile(char* fileName) {
-
-    FILE *in = fopen(fileName, "r");
-	if(!in) {
-		printf("Error in opening input file...exiting with error code -1\n");
-		return -1;
-	}
-    int i, j;
-
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 4; j++) {
-            fscanf(in," %c", &maximum[i][j]);
-        }
+    FILE* in = fopen(fileName, "r");
+    if (!in) {
+        printf("Error in opening input file...exiting with error code -1\n");
+        return -1;
     }
-    // Testing 
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 4; j++) {
-            printf(" %c", maximum[i][j]);
+    const char *delim = ",\n\t ";
+    char line[20];
+    char* token;
+    int i, n, j; 
+    i = 0;
+    // Read file line by line
+    while (fgets(line, sizeof(line), in)) {
+        j = 0;
+        // Initalize line tokens 
+        token = strtok(line, delim);
+        // While not empty 
+        printf("\n");
+        while (token != NULL) {
+            n = atoi(token);
+            maximum[i][j] = n;
+            printf(" %d", maximum[i][j]);
+            j++;
+            token = strtok(NULL, delim);
         }
+        i++;
     }
     return 0;
 }
